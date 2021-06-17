6a4040e30d6b77e11e41ae71d444ac3aef4933ac
destrkast <destrkast29@gmail.com>
2021-06-17 14:55:18 +0300

    настройка сервера

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+app.use(requireHTTPS);
+app.use(express.static('./dist/oblako-front'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: 'dist/oblako-front/'})
+})
+
+app.listen(port)
