c31d1b5e4cb642cc52d835eaf89d8de4ff8a8a2c
destrkast <destrkast29@gmail.com>
2021-06-25 10:17:35 +0300

    adding of header

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+const MaterialComponents = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
+];
+
+
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+
+export class MaterialModule { }
